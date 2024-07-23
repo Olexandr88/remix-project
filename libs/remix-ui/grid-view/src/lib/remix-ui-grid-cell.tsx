1b678d028b1b8b511a008c72b46098553e93d3b9
lianahus <liana@ethereum.org>
2024-07-23 13:09:48 +0200

    fixing filter for providers

--- libs/remix-ui/grid-view/src/lib/remix-ui-grid-cell.tsx
+++ libs/remix-ui/grid-view/src/lib/remix-ui-grid-cell.tsx
@@ -41,7 +41,7 @@ export const RemixUIGridCell = (props: RemixUIGridCellProps) => {
     // check for tags
     if (props.tagList && props.tagList.length != 0) {
       enabled = props.tagList.some((key) => filterCon.keyValueMap[key]?.enabled)
-    } else if (filterCon?.keyValueMap['no tag']?.enabled) {
+    } else if (filterCon?.keyValueMap['no tag']?.enabled || !Object.keys(filterCon?.keyValueMap).length) {
       enabled = true
     }
 
